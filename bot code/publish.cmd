c7cc8e3d52d97ecb43871fdd0e896ab29860ea84
VarshithReddyB <112534655+VarshithReddyB@users.noreply.github.com>
2022-08-31 17:09:09 +0530

    Add files via upload

---
+++ bot code/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\faqsoncovid19in-bot-Web-Deploy.pubxml -p:Password=LwBE737KqJo6pZi0ohorhYtg7mxxZjFf14Qq3NKYH9zzspzSy7qhYgbbFmf6
+
